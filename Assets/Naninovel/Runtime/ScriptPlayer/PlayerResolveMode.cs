80a347fbd9e07f9e9b1f4ae00777dc84611bb7a9
Анунах великий <guk.012v@gmail.com>
2024-08-06 23:32:06 +0300

    123

---
+++ Assets/Naninovel/Runtime/ScriptPlayer/PlayerResolveMode.cs
@@ -0,0 +1,23 @@
+// Copyright 2023 ReWaffle LLC. All rights reserved.
+
+namespace Naninovel
+{
+    /// <summary>
+    /// The mode in which script player handles missing playback spots when loading state.
+    /// </summary>
+    public enum PlayerResolveMode
+    {
+        /// <summary>
+        /// Attempt to play from nearest next spot; in case no next spots, start from nearest previous one.
+        /// </summary>
+        Nearest,
+        /// <summary>
+        /// Start playing current script from start.
+        /// </summary>
+        Restart,
+        /// <summary>
+        /// Throw an error.
+        /// </summary>
+        Error
+    }
+}
